710caa6407d9adc023057d3e3683ce2eb80a3732
AndreiLukashevich <rid7003482@gmail.com>
2016-11-10 00:59:48 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ function something()
 
   var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-  x = x*1 + 1; // x = x + 1
+  x = x * 1 + 1; // x = x + 1
 
   window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
@@ -12,5 +12,9 @@ function something()
 
 function add_to_cart(id)
 {
-  alert('You added pizza with id:' + id);
+  var key = 'product_' + id;
+  
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
