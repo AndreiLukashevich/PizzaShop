017b84be69fef6cc0467b107a0bfdebbeab21025
AndreiLukashevich <rid7003482@gmail.com>
2016-11-09 22:40:02 +0300

    update migration add_product

--- db/migrate/20161109174115_add_products.rb
+++ db/migrate/20161109174115_add_products.rb
@@ -1,9 +1,36 @@
 class AddProducts < ActiveRecord::Migration
   def change
-    Product.create :title => 'Hawaiian', :description => 'Nice Hawaiian pizza', :price => 12, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/hawaiian.jpg'
+    Product.create ({
+    :title => 'Hawaiian',
+    :description => 'Nice Hawaiian pizza',
+    :price => 12,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => false,
+    :is_best_offer => true,
+    :path_to_image => '/images/hawaiian.jpg'
+    })
 
-    Product.create :title => 'Pepperoni', :description => 'This is Pepperoni pizza', :price => 14, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
+    Product.create ({
+    :title => 'Pepperoni',
+    :description => 'This is Pepperoni pizza',
+    :price => 14,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => false,
+    :is_best_offer => true,
+    :path_to_image => '/images/pepperoni.jpg'
+    })
 
-    Product.create :title => 'Vegetarian', :description => 'Amazing Vegetarian pizza', :price => 10, :size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/veg.jpg'
+    Product.create ({
+    :title => 'Vegetarian',
+    :description => 'Amazing Vegetarian pizza',
+    :price => 10,
+    :size => 30,
+    :is_spicy => false,
+    :is_veg => true,
+    :is_best_offer => false,
+    :path_to_image => '/images/veg.jpg'
+    })
   end
 end
